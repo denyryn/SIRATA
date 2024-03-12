5f8db9291d3f0d2f990ddfc93d035736fd02c8fa
Rakha Yuda <rakhayuda04@gmail.com>
2024-03-12 10:55:12 +0000

    blm ada foreign key

---
+++ database/migrations/2024_03_12_102256_tabel_surat.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('Surat', function (Blueprint $table) {
+            $table->integer('id_surat')->primary();
+            $table->integer('nim_mahasiswa');
+            $table->integer('id_perihal');
+            $table->integer('id_pejabat');
+            $table->integer('id_status');
+            $table->integer('id_kategori_surat');
+            $table->string('tujuan_surat', 50);
+            $table->integer('nomor_surat');
+            $table->string('email_mahasiswa', 50);
+            $table->datetime('tanggal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
