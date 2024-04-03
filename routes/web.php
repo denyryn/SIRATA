6725ea735af04a24f98058f3f1e81b886a02c7c7
Ryunoo09 <rakhayuda04@gmail.com>
2024-04-03 11:39:58 +0700

    y

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\UserMahasiswaController;
 use App\Http\Controllers\ProgramStudiController;
 use App\Http\Controllers\UserAdminController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\JabatanController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\PerihalController;
+use App\Http\Controllers\StatusController;
 
 
 /*
@@ -31,8 +36,7 @@ Route::prefix('mahasiswa')->group(function () {
 
 // Admin Routes
 Route::prefix('admin')->group(function () {
-    Route::get('/dashboard', [userAdmin::class, 'index'])->name("admin.index");
-    Route::get('/dashboard/program_studi', [ProgramStudi::class, 'index'])->name("admin.prodi");
+    Route::get('/dashboard', [UserAdminController::class, 'index'])->name("admin.index");
 
     // Program Studi Routes
     Route::prefix('/dashboard/program_studi')->group(function () {
@@ -41,6 +45,8 @@ Route::prefix('admin')->group(function () {
         Route::put('/{id_prodi}', [ProgramStudiController::class, 'update'])->name("prodi.update");
         Route::delete('/{id_prodi}', [ProgramStudiController::class, 'delete'])->name("prodi.delete");
     });
+
+    // Jabatan Routes
     Route::prefix('/dashboard/jabatan')->group(function () {
         Route::get('/', [JabatanController::class, 'index'])->name("admin.jabatan");
         Route::post('/', [JabatanController::class, 'store'])->name("jabatan.store");
@@ -48,4 +54,37 @@ Route::prefix('admin')->group(function () {
         Route::delete('/{id_jabatan}', [JabatanController::class, 'delete'])->name("jabatan.delete");
     });
 
+    // Perihal Routes
+    Route::prefix('/dashboard/perihal')->group(function () {
+        Route::get('/', [PerihalController::class, 'index'])->name("admin.perihal");
+        Route::post('/', [PerihalController::class, 'store'])->name("perihal.store");
+        Route::put('/{id_perihal}', [PerihalController::class, 'update'])->name("perihal.update");
+        Route::delete('/{id_perihal}', [PerihalController::class, 'delete'])->name("perihal.delete");
+    });
+    Route::prefix('/dashboard/kategori_surat')->group(function () {
+        Route::get('/', [KategoriController::class, 'index'])->name("admin.kategori");
+        Route::post('/', [KategoriController::class, 'store'])->name("kategori.store");
+        Route::put('/{id_kategori}', [KategoriController::class, 'update'])->name("kategori.update");
+        Route::delete('/{id_kategori}', [KategoriController::class, 'delete'])->name("kategori.delete");
+    });
+
+    Route::prefix('/dashboard/status')->group(function () {
+        Route::get('/', [StatusController::class, 'index'])->name("admin.status");
+        Route::get('/{nama_status}', [StatusController::class, 'show'])->name("status.show");
+        Route::get('/create', [StatusController::class, 'create'])->name("status.create");
+        Route::post('/', [StatusController::class, 'store'])->name("status.store");
+        Route::put('/{id_status}', [StatusController::class, 'update'])->name("status.update");
+        Route::delete('/{id_status}', [StatusController::class, 'delete'])->name("status.delete");
+    });
+
+    // Route::prefix('/dashboard/perihal')->group(function () {
+    //     Route::get('/', [PerihalController::class, 'index'])->name("admin.perihal");
+    //     Route::get('/{nama_perihal}', [PerihalController::class, 'show'])->name("perihal.show");
+    //     Route::get('/create', [PerihalController::class, 'create'])->name("perihal.create");
+    //     Route::post('/', [PerihalController::class, 'store'])->name("perihal.store");
+    //     Route::put('/{id_perihal}', [PerihalController::class, 'update'])->name("perihal.update");
+    // });
+
 });
+
+
